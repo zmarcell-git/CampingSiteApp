a17b76cadfa36175263479726e2e62c35db9585e
zmarcell-git <marcellzudor99@gmail.com>
2025-12-07 18:35:57 +0100

    Refactor for Single Responsibility

--- src/Service/CampingSiteManager.java
+++ src/Service/CampingSiteManager.java
@@ -6,7 +6,6 @@ import java.util.Scanner;
 
 import Model.CampingSite;
 import Model.CampingType;
-import Model.User;
 
 public class CampingSiteManager {
     private ArrayList<CampingSite> campingSites = new ArrayList<CampingSite>();
@@ -39,6 +38,15 @@ public class CampingSiteManager {
     }
 
     /**
+     * Prints the site list into the UI
+     */
+    public void printCampingSites() {
+        for (CampingSite site : campingSites) {
+            System.out.println(site.toString());
+        }
+    }
+
+    /**
      * Converts a given string to the corresponding CampingType enum value.
      *
      * <p>
@@ -66,57 +74,6 @@ public class CampingSiteManager {
     }
 
     /**
-     * Provides a menu for modifying different attributes of a camping site
-     * identified by the given ID.
-     * <p>
-     * The method checks whether the camping site exists. If it does not, an error
-     * message is displayed
-     * and the method exits. Otherwise, a menu is shown where the user can choose to
-     * modify:
-     * <ul>
-     * <li>Capacity</li>
-     * <li>Price</li>
-     * <li>Status</li>
-     * <li>Amenities</li>
-     * </ul>
-     * Based on the selected option, the corresponding modification method is
-     * called.
-     *
-     * @param id The ID of the camping site to modify.
-     */
-    public void ModifyCampingSiteById(String id) {
-        if (findCampingSiteById(id) == null) {
-            System.out.println("Nincs ilyen kempinghely!");
-            return;
-        }
-
-        System.out.println("Válasszon az alábbiak közül, hogy mit szeretne módosítani: ");
-        System.out.println(
-                "1. Kapacitás változtatása\n2. Ár változtatása\n3. Státusz változtatása\n4. Extrák változtatása");
-
-        String menuOption = sc.nextLine().trim().toLowerCase();
-
-        switch (menuOption) {
-            case "1":
-                ModifyCapacity(id);
-                break;
-            case "2":
-                ModifyPrice(id);
-                break;
-            case "3":
-                ModifyStatus(null, id); // TODO: implement user permission
-                break;
-            case "4":
-                ModifyAmenities(id);
-                break;
-            default:
-                System.out.println(
-                        "Nincs ilyen menü opció: " + menuOption + "\nKérem válasszon egy érvénye opciót!(1,2,3,4)");
-                break;
-        }
-    }
-
-    /**
      * Modifies the capacity of a camping site identified by the given ID.
      * <p>
      * The method retrieves the camping site and displays its current capacity.
@@ -126,73 +83,13 @@ public class CampingSiteManager {
      *
      * @param id The ID of the camping site whose capacity will be modified.
      */
-    private void ModifyCapacity(String id) {
-        CampingSite site = findCampingSiteById(id);
-        System.out.println(site.getId() + " kempinghely jelenlegi kapacitása: " + site.getCapacity());
-        String input = sc.nextLine().trim();
+    public void ModifyCapacity(CampingSite campingSite, String newCapacity) {
         try {
-            int newCapacity = Integer.parseInt(input);
-            site.setCapacity(newCapacity);
+            int capacityToInteger = Integer.parseInt(newCapacity);
+            campingSite.setCapacity(capacityToInteger);
         } catch (NumberFormatException e) {
             System.out.println("Kérem egész számot adjon meg a kapacitás változtatásához! " + e.getMessage());
         }
-    }
-
-    /**
-     * Modifies the price of a camping site identified by the given ID.
-     * <p>
-     * The method retrieves the camping site and displays its current price.
-     * It then reads user input and attempts to convert it to a double value.
-     * If the conversion is successful, the site's price is updated.
-     * If the input is not a valid number, an error message is shown.
-     *
-     * @param id The ID of the camping site whose price will be modified.
-     */
-    private void ModifyPrice(String id) {
-        CampingSite site = findCampingSiteById(id);
-        System.out.println(site.getId() + " kempinghely jelenlegi ára: " + site.getPrice());
-        String inputPrice = sc.nextLine().trim();
-        try {
-            double newPrice = Double.parseDouble(inputPrice);
-            site.setPrice(newPrice);
-        } catch (NumberFormatException e) {
-            System.err.println("Nem megfelelő formátumba megadott ár! " + e.getMessage());
-        }
-    }
-
-    /**
-     * Sets the site pricing dynamic with helper methods. Find the site by id.
-     * 
-     * @param id given id to find the site
-     */
-    public void setPricingToDynamic(String id) {
-        CampingSite site = findCampingSiteById(id);
-        System.out.println("Adja meg milyenre akarja változtatni az árazást: ");
-        System.out.println("1. Fix árazás\n2. Extráktól függő árazás\n3. Szezonális árazás");
-        String input = sc.nextLine();
-        switch (input) {
-            case "1":
-                SiteFixPricing(site);
-                break;
-            case "2":
-                SitePricingToGrowWithAmenities(site);
-                break;
-            case "3":
-                SitePricingSeasonal(site);
-                break;
-
-            default:
-                break;
-        }
-    }
-
-    /**
-     * Sets pricing of the given site seasonal
-     * 
-     * @param site the site which needs to be changed
-     */
-    private void SitePricingSeasonal(CampingSite site) {
-        // TODO: implement
     }
 
     /**
@@ -215,11 +112,13 @@ public class CampingSiteManager {
      * 
      * @param site the object which has to be changed
      */
-    private void SiteFixPricing(CampingSite site) {
-        System.out.print("Mennyi legyen az új ára a kempinghelynek? ");
-        String input = sc.nextLine().trim();
-        double newprice = Integer.parseInt(input);
-        site.setPrice(newprice);
+    public void SiteFixPricing(CampingSite site, String newPrice) {
+        try {
+            double newprice = Double.parseDouble(newPrice);
+            site.setPrice(newprice);
+        } catch (NumberFormatException e) {
+            System.err.println("Kérem számmal adja meg az új árat! " + e.getMessage());
+        }
         System.out.println("Ár sikeresen változtatva: " + site.getId() + " kempinghely ára: " + site.getPrice());
     }
 
@@ -228,10 +127,10 @@ public class CampingSiteManager {
      * 
      * @param site site to be changed
      */
-    private void SitePricingToGrowWithAmenities(CampingSite site) {
-        double price = 500;
-        price = price + (100 * site.getAmenities().size());
-        site.setPrice(price);
+    public void SitePricingToGrowWithAmenities(CampingSite site, String basePrice, String extrasPrice) {
+        double convertedBasePrice = Double.parseDouble(basePrice);
+        convertedBasePrice += (Double.parseDouble(extrasPrice) * site.getAmenities().size());
+        site.setPrice(convertedBasePrice);
         System.out.println(site.getId() + " kempinghely új árazása: " + site.getPrice()
                 + "/éj az extrák mennyisége miatt: " + site.getAmenities().size());
     }
@@ -244,19 +143,12 @@ public class CampingSiteManager {
      * using the provided user object for permission checking. If an error occurs
      * during the update, the exception message is displayed.
      *
-     * @param curretnUser The user who is attempting to change the site's status.
-     * @param id          The ID of the camping site whose status will be modified.
+     * @param id The ID of the camping site whose status will be modified.
      */
-    private void ModifyStatus(User curretnUser, String id) {
-        CampingSite site = findCampingSiteById(id);
-        System.out.println("Kempinghely jelenlegi státusza: " + site.getStatus());
-        System.out.print("Mire szeretné változtatni a státuszát a kempinghelynek?: ");
-        String newStatus = sc.nextLine().trim().toLowerCase();
-        try {
-            site.setStatus(curretnUser, newStatus);
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
-        }
+    public void ModifyStatus(CampingSite site, String status) {
+        site.setStatus(status);
+        System.out.println("Kempinghely státusza sikeresen módosítva! " + site.getId() + " kempinghely új státusza: "
+                + site.getStatus());
     }
 
     /**
@@ -322,6 +214,36 @@ public class CampingSiteManager {
         }
     }
 
+    public void addAmenitieForSite(CampingSite site, String amenitie) {
+        for (String s : site.getAmenities()) {
+            if (s.equals(amenitie)) {
+                System.out.println("Ez az extra már megtalálható ezen a kempinghelyen: " + amenitie);
+                return;
+            }
+        }
+        site.addAmenitie(amenitie);
+        System.out.println("Extra sikeresen hozzáadva a kempinghelyhez! Jelenlegi extrák: " + site.getAmenities());
+    }
+
+    public void deleteAmenitiesFromSite(CampingSite site, String delExtra) {
+        for (String s : site.getAmenities()) {
+            if (s.equals(delExtra)) {
+                site.deleteAmenities(delExtra);
+                System.out.println("Sikeresen törölte az extrák közül: " + delExtra);
+                return;
+            }
+        }
+        System.out.println("Nincs ilyen extra a kempinghely extrái között: " + delExtra);
+    }
+
+    public void deleteAllAmenitiesFromSite(CampingSite site) {
+        if (site.getAmenities().size() == 0) {
+            System.out.println("Ezen a kempinghelyen még nincs extra!");
+        }
+        site.deleteAllAmenities();
+        System.out.println("Sikeresen törölte a kempinghely összes extráját!");
+    }
+
     /**
      * Removes a camping site by the given ID from the camping sites.
      * 
@@ -343,7 +265,7 @@ public class CampingSiteManager {
      * @param campId Strign Campsite.id
      * @return Returns Campsite.
      */
-    private CampingSite findCampingSiteById(String campId) {
+    public CampingSite findCampingSiteById(String campId) {
         for (CampingSite campingSite : campingSites) {
             if (campingSite.getId().equals(campId)) {
                 return campingSite;
